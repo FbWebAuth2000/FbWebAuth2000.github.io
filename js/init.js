6d22954dacd7775eac68b1ea6b90e609ccce2eeb
FbWebAuth2000 <84884659+FbWebAuth2000@users.noreply.github.com>
2021-05-26 20:42:47 -0500

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyDV_s1RrZrr_3GsUOVXIZOaOXQPn-TO43Y",
+    authDomain: "fbwebauth20000.firebaseapp.com",
+    projectId: "fbwebauth20000",
+    storageBucket: "fbwebauth20000.appspot.com",
+    messagingSenderId: "867899623343",
+    appId: "1:867899623343:web:762fd02b97a7024bb5115a",
+    measurementId: "G-CVEX0C1ETP"
+  });
